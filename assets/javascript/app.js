93a2efcbff3505c363cfdff49f4dd43195018d1d
Michael Hoffman <maaronhoffman@gmail.com>
2020-05-22 11:12:58 -0700

    math fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,8 +55,18 @@ $( document ).ready(function() {
     //After Data is added push to that table from the FireBase
     database.ref().on("child_added", function(childSnapshot) {
 
+        
+        $firstTime = childSnapshot.val().firstTime
+        $trainFreq = childSnapshot.val().freq
     
-    $("#train-form").append("<tr><td class='text-center'>" + childSnapshot.val().name +  "</td><td class='text-center'>" + childSnapshot.val().arrival +  "</td><td class='text-center'>" + childSnapshot.val().mins +  "</td></tr>")
+    
+        var convertTime = moment($firstTime, "HH:mm").subtract(1, "years");  	 
+        var tDif = moment().diff(moment(convertTime), 'minutes'); 	
+        var remainder = tDif % $trainFreq; 	 
+        $minAway = $trainFreq - remainder; 	
+        $nextArrival = moment().add($minAway, "m").format("hh:mm A"); 
+    
+        $("#train-form").append("<tr><td class='text-center'>" + childSnapshot.val().name +  "</td><td class='text-center'>" + $nextArrival +  "</td><td class='text-center'>" + $minAway +  "</td></tr>")
     
     });
 
